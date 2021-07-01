f4a9f6218e82047812e35509185297ba7484d704
Darina <to.darina.to>
2021-07-01 14:06:08 +0300

    Initial project structure

---
+++ src/main/java/mate/util/ConnectionUtil.java
@@ -0,0 +1,28 @@
+package mate.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    public static final String URL = "url";
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+    public static final String JDBC_DRIVER = "driver";
+
+    static {
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't find SQL Driver", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        Properties dbProperties = new Properties();
+        dbProperties.setProperty("user", USERNAME);
+        dbProperties.setProperty("password", PASSWORD);
+        return DriverManager.getConnection(URL, dbProperties);
+    }
+}
